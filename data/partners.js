60cbfe54df6638dc792fa9ef27cef8884b29e5e9
neepunraj <neepushre97@gmail.com>
2025-06-02 17:31:51 +0200

    homeWork

---
+++ data/partners.js
@@ -0,0 +1,32 @@
+export const partners = [
+  {
+    id: 1,
+    name: "Amazon",
+    logo: "/business_partners/amazon_logo.png",
+  },
+  {
+    id: 2,
+    name: "Alphabet",
+    logo: "/business_partners/alphabet-logo.png",
+  },
+  {
+    id: 3,
+    name: "NYU",
+    logo: "/business_partners/nyu-logo.png",
+  },
+  {
+    id: 4,
+    name: "CBC",
+    logo: "/business_partners/CBC_Logo_White.png",
+  },
+  {
+    id: 5,
+    name: "Queen's University",
+    logo: "/business_partners/QueensLogo_white.png",
+  },
+  {
+    id: 6,
+    name: "Samsung",
+    logo: "/business_partners/samsung-logo.png",
+  },
+];
---
+++ data/values.js
@@ -0,0 +1,22 @@
+export const values = [
+  {
+    id: 1,
+    title: "Love your craft.",
+    desc: "We’re here because we love what we do. It’s our passion, hobby, career, and vocation. We’re looking for others who feel the same way.",
+  },
+  {
+    id: 2,
+    title: "Be bold. Be humble.",
+    desc: "There is no top-down. We lead from the bottom and the edges. Everyone drives. We celebrate ideas over egos, where the best idea wins, regardless of who or where it comes from.",
+  },
+  {
+    id: 3,
+    title: "Be better. Always push forward.",
+    desc: "We’re on a tireless pursuit to become better and expect those around us to do the same.",
+  },
+  {
+    id: 4,
+    title: "Be a Professional",
+    desc: "There’s important work to be done. Speak up. Be present. Provide value, with honesty and candour.",
+  },
+];
